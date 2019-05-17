7ab243c08d9018547c4f8a10f01561b60db9d5c3
tannerkoscinski <tannerkoscinski@gmail.com>
2019-05-17 00:06:19 -0700

    .

--- grouper.py
+++
@@ -1,9 +0,0 @@
-from functions import searchartist, searchalbums, getalbum, searchplaylist
-from functions import getplaylist, getanalysis
-
-# searchalbums('beartooth')
-# getalbum('4xtHySb6XaNo8SLRgfpUSu', 'Skinny Dipping', 'Stand Atlantic')
-# getanalysis('Skinny Dipping')
-# searchplaylist('Pop Punk\'s Not Dead')
-# getplaylist('spotify', '37i9dQZF1DXcBkxDaAQX0a', 'Punk Unplugged')
-# getanalysis('Punk Unplugged')
